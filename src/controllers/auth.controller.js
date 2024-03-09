15a253606b3494c92e80deb54f60f228640ca230
Mitesh Gulecha <miteshgulecha@gmail.com>
2024-03-09 16:49:26 -0500

    access token fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const register = async (req,res,next)=>{
                 email:newUser.email,
                 picture:newUser.picture,
                 status:newUser.status,
-                access_token,
+                token:access_token,
             }
         })
     }catch(error){
@@ -60,7 +60,7 @@ export const login = async (req,res,next)=>{
                 email:user.email,
                 picture:user.picture,
                 status:user.status,
-                access_token,
+                token:access_token,
             }
         })
     }catch(error){
